c932914c49d133208f790d1701f8324d3448c2ed
castillo05 <jorgeantonio20142014@gmail.com>
2025-05-09 19:10:01 -0600

    feat: environment configuration

---
+++ atom-frontend/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://us-central1-atom-df39d.cloudfunctions.net/api/api',
+    firebase: {
+        projectId: 'atom-df39d',
+        appId: '1:123456789:web:abcdef123456789',
+        storageBucket: 'atom-df39d.appspot.com',
+        apiKey: 'your-api-key',
+        authDomain: 'atom-df39d.firebaseapp.com',
+        messagingSenderId: '123456789',
+    }
+}; 
